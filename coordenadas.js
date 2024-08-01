4cbcf714b35b0cf5545917a1529365e3bbd8d37d
NARMUMNG <157733705+NARMUMNG@users.noreply.github.com>
2024-08-01 13:31:31 -0500

    Añadi parametros para poder reutilizar el script en cualquier lenguaje

--- coordenadas.js
+++ coordenadas.js
@@ -1,23 +1,36 @@
-function convertirAPolares() {
-    const radio = parseFloat(document.getElementById('radio').value);
-    const anguloGrados = parseFloat(document.getElementById('angulo').value);
+function convertirAPolares(radio, anguloGrados) {
     const anguloRadianes = anguloGrados * Math.PI / 180;
 
     const x = radio * Math.cos(anguloRadianes);
     const y = radio * Math.sin(anguloRadianes);
 
-    document.getElementById('resultadoRectangular').innerText = 
-        `Coordenadas Rectangulares: X = ${x.toFixed(2)}, Y = ${y.toFixed(2)}`;
+    return { x: x.toFixed(2), y: y.toFixed(2) };
 }
 
-function convertirARectangulares() {
-    const x = parseFloat(document.getElementById('x').value);
-    const y = parseFloat(document.getElementById('y').value);
-
+function convertirARectangulares(x, y) {
     const radio = Math.sqrt(x * x + y * y);
     const anguloRadianes = Math.atan2(y, x);
     const anguloGrados = anguloRadianes * 180 / Math.PI;
 
+    return { radio: radio.toFixed(2), anguloGrados: anguloGrados.toFixed(2) };
+}
+
+function manejarConversionPolares() {
+    const radio = parseFloat(document.getElementById('radio').value);
+    const anguloGrados = parseFloat(document.getElementById('angulo').value);
+
+    const coordenadasRectangulares = convertirAPolares(radio, anguloGrados);
+
+    document.getElementById('resultadoRectangular').innerText = 
+        `Coordenadas Rectangulares: X = ${coordenadasRectangulares.x}, Y = ${coordenadasRectangulares.y}`;
+}
+
+function manejarConversionRectangulares() {
+    const x = parseFloat(document.getElementById('x').value);
+    const y = parseFloat(document.getElementById('y').value);
+
+    const coordenadasPolares = convertirARectangulares(x, y);
+
     document.getElementById('resultadoPolar').innerText = 
-        `Coordenadas Polares: r = ${radio.toFixed(2)}, θ = ${anguloGrados.toFixed(2)}°`;
+        `Coordenadas Polares: r = ${coordenadasPolares.radio}, θ = ${coordenadasPolares.anguloGrados}°`;
 }
